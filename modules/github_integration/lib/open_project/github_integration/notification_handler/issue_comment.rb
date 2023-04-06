4234c39ce932df39e029d4b5b7127f6cb8d40e28
Oliver Günther <mail@oliverguenther.de>
2023-04-06 09:46:21 +0200

    Fix specs generating macros

--- modules/github_integration/lib/open_project/github_integration/notification_handler/issue_comment.rb
+++ modules/github_integration/lib/open_project/github_integration/notification_handler/issue_comment.rb
@@ -47,7 +47,7 @@ module OpenProject::GithubIntegration
         github_system_user = User.find_by(id: payload.open_project_user_id)
         work_packages = find_mentioned_work_packages(payload.comment.body, github_system_user)
 
-        new_work_packages = without_already_referenced(work_packages, pull_request)
+        new_work_packages = without_already_referenced(work_packages, pull_request&.work_packages.to_a || [])
         return unless new_work_packages.any?
 
         pull_request = upsert_partial_pull_request(new_work_packages)
